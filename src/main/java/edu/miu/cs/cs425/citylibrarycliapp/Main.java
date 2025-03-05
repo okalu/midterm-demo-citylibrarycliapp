09cd68e044e96dbb7eec4af719577a9fcd6e4ea4
Prof OKalu <okalu1@outlook.com>
2025-03-05 12:31:21 -0600

    New project for LibraryCLIApp

---
+++ src/main/java/edu/miu/cs/cs425/citylibrarycliapp/Main.java
@@ -0,0 +1,18 @@
+package edu.miu.cs.cs425.citylibrarycliapp;
+
+import edu.miu.cs.cs425.citylibrarycliapp.model.Publisher;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var welcomeMessage = "Welcome to Cs425 City Library";
+        System.out.printf("%s\n", welcomeMessage);
+        var publishers = List.of(
+                new Publisher(101, "McGraw-Hill", "sales@mcgraw.com", null),
+                new Publisher(102, "Apress", "sales@apress.com", "(123) 456-7890"),
+        new Publisher(103, "Penguin-RandomHouse", null, null)
+        );
+        publishers.forEach(System.out::println);
+    }
+}
